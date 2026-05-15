cddd75dd58f85efaae267af040ea1a3d33b0f930
SamTF <elseban88@gmail.com>
2026-05-15 21:40:16 +0200

    Schema: Add Language field to Film
    - Add Language string field

--- film.go
+++ film.go
@@ -9,15 +9,17 @@ type Film struct {
 	Genre    string `json:"genre"`
 	Plot     string `json:"plot"`
 	Poster   string `json:"poster"`
+	Language string `json:"language"`
 }
 
-func NewFilm(title, year, runtime, director, country, genre, plot, poster string) *Film {
+func NewFilm(title, year, runtime, director, country, language, genre, plot, poster string) *Film {
 	return &Film{
 		Title:    title,
 		Year:     year,
 		Runtime:  runtime,
 		Director: director,
 		Country:  country,
+		Language: language,
 		Genre:    genre,
 		Plot:     plot,
 		Poster:   poster,
